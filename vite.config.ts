1349d25dc4dc8174da30e6d1374377f7a3021604
laurene-wat <contact@laurene-wat.com>
2025-12-17 11:26:55 +0100

    Basculer sur vite-plugin-ssg pour optimisation SEO

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import Prerender from 'vite-plugin-prerender';
+import { ViteSSG } from 'vite-plugin-ssg';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
@@ -10,17 +10,12 @@ export default defineConfig(({ mode }) => {
     server: { port: 3000, host: '0.0.0.0' },
     plugins: [
       react(),
-      Prerender({
-        routes: ['/'],
-        staticDir: 'dist',
-        renderAfterDocumentEvent: 'app-mounted',
-        rendererOptions: {
-          // Attend que les ressources réseau se calment
-          renderAfterTime: 0,
-          maxConcurrentRoutes: 1
-        },
-        headless: true
-      }),
+      // Active SSG sur la route "/"
+      ViteSSG({
+        script: 'async',
+        formatting: 'minify',
+        includedRoutes: () => ['/']
+      })
     ],
     base: '/',
     define: {
